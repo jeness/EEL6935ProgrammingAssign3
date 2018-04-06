eef36b262d222f8b2bf729271156ccb65b0026e2
jeness <yuhaoran9968@gmail.com>
2018-04-05 23:11:23 -0400

    finish X and Y data process

--- assign3.py
+++ assign3.py
@@ -4,6 +4,8 @@ import numpy as np
 from string import maketrans
 import string
 trainlist=[]
+X = []
+Y = []
 DNAdict = {'A':1,'C':2,'G':3,'T':4}
 with open('train.csv','rb') as traincsvFile:
     lines=csv.reader(traincsvFile)
@@ -17,13 +19,27 @@ with open('train.csv','rb') as traincsvFile:
     translatetable = maketrans('ACGT', '1234')
     print trainlist
     for eachrow in trainlist:
+        eachrow[0] = string.atoi(eachrow[0])
         # print 'eachrow=' + eachrow[1]
-        eachrow[1] = string.atoi(eachrow[1].translate(translatetable))
-        # print 'new_eachrow=' + eachrow[1]
+        eachrow[1] = eachrow[1].translate(translatetable)
+        #print 'new_eachrow=' + eachrow[1]
+        Xstring = '-'.join(eachrow[1]).split('-')
+        Xstring = map(eval, Xstring)
+        #print Xstring
+        X.append(Xstring)
         eachrow[2] = string.atoi(eachrow[2])
+        Y.append(eachrow[2])
     print trainlist
-    # l.append(line)
-    # l.remove(l[0])
-    # print l
+    print Y
+    print X
+#-----------------------------------begin train model
+    from keras.models import Sequential
+    from keras.layers import Dense
+    import numpy
+
+    # fix random seed for reproducibility
+    seed = 7
+    numpy.random.seed(seed)
+
 
     # X= dataset[:,0:8]
